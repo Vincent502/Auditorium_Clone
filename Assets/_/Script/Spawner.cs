1306869849459803e32f7d0597cf79d63cb92b1e
Vincent <v1996.vincent@gmail.com>
2026-05-18 14:07:52 +0200

    Initial project

---
+++ Assets/_/Script/Spawner.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject m_particule;
+    public float m_timeBetweenParticle;
+    public float m_unitCircleMultiplier;
+
+    private void Update()
+    {
+        
+    }
+
+
+}
